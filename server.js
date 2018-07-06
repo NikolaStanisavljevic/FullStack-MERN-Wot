77d7fe24d3138795076eda6fe8c82ee28d56edfc
Nikola Stanisavljevic <marinko.magla91@gmail.com>
2018-07-06 03:28:52 +0200

    Vesion Alpha 1.2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const items = require('./routes/api/items');
 const path = require('path');
 
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
     .then(()=> console.log('MongoDB Connected..'))
     .catch(err=> console.log(err));
     
-app.use(cors({origin: 'http://localhost:3000'}));
+// app.use(cors({origin: 'http://localhost:3000'}));
 
 // Use routes
 app.use('/api/items', items);
