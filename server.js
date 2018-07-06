929a4f1c063ce0c7c6d346cce4931b4a981fa1ca
Nikola Stanisavljevic <marinko.magla91@gmail.com>
2018-07-06 03:24:57 +0200

    Vesion Alpha 1.1

--- server.js
+++ server.js
@@ -25,14 +25,14 @@ app.use(cors({origin: 'http://localhost:3000'}));
 app.use('/api/items', items);
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-
-    app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+  }
 
 const port = process.env.PORT || 5000;
 
